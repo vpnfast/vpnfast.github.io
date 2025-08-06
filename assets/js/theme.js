a54ceace84d48711cdf7e6dfca8787322698a3ef
= <topvpntool@gmail.com>
2025-08-06 23:28:48 +1000

    update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,11 +1,11 @@
 
       (function ($) {"use strict";
-     
+      
       $(function () {
         var header = $(".start-style");
         $(window).scroll(function () {
           var scroll = $(window).scrollTop();
-   
+    
           if (scroll >= 10) {
             header.removeClass('start-style').addClass("scroll-on");
           } else {
@@ -13,15 +13,15 @@
           }
         });
       });
-   
+    
       //Animation
-   
+    
       $(document).ready(function () {
         $('body.hero-anime').removeClass('hero-anime');
       });
-   
+    
       //Menu On Hover
-   
+    
       $('body').on('mouseenter mouseleave', '.nav-item', function (e) {
         if ($(window).width() > 750) {
           var _d = $(e.target).closest('.nav-item');_d.addClass('show');
@@ -30,9 +30,9 @@
           }, 1);
         }
       });
-   
-   
-   
+    
+    
+    
     })(jQuery);
 
 // Blank Target External Links
